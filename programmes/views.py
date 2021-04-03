189b97925a2c5899a35305ea7b4827295275a13b
martinjohnson26 <martin.johnson26@outlook.com>
2021-04-03 09:04:00 +0000

    added programme views and templates

--- programmes/views.py
+++ programmes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Programme
 
 # Create your views here.
+
+
+def all_programmes(request):
+    """ Displays all programmes and allows search and sorting functions """
+
+    programmes = Programme.objects.all()
+
+    context = {
+        'programmes': programmes,
+    }
+
+    return render(request, 'programmes/programmes.html', context)
