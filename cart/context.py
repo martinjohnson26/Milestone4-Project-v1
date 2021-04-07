bbd2d4fc108589f50ed5865dfbf12fe8cbf41bf3
martinjohnson26 <martin.johnson26@outlook.com>
2021-04-07 13:22:08 +0000

    updated context processor

--- cart/context.py
+++ cart/context.py
@@ -1,12 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from programmes.models import Programme
 
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    programme_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        programme = get_object_or_404(Programme, pk=item_id)
+        total += quantity * programme.price
+        programme_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'programme': programme,
+        })
 
     if total < settings.FREE_POSTAGE_TRIGGER:
         postage = total * Decimal(settings.STANDARD_POSTAGE_PERCENTAGE / 100)
@@ -21,7 +34,7 @@ def cart_contents(request):
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'programme_count': programme_count,
         'postage': postage,
         'free_postage_delta': free_postage_delta,
         'free_postage_trigger': settings.FREE_POSTAGE_TRIGGER,
