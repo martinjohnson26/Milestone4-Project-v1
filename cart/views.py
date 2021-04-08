4cae92212bf75ede2f95efcaa7cb913afe30a869
martinjohnson26 <martin.johnson26@outlook.com>
2021-04-08 16:36:35 +0000

    reviewed code from last push and corrected error in cart.html line 145 to make delete option work

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
+
 # Create your views here.
 from programmes.models import Programme
 
@@ -13,7 +14,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified programme to the shopping cart """
 
-    programme = Programme.objects.get(pk=item_id)
+    programme = get_object_or_404(Programme, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -31,15 +32,16 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """ Adjust quantity of the specified programme in the shopping cart """
 
+    programme = get_object_or_404(Programme, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
-        messages.success(request, f'Updated {programme.fixture} quantity to {bag[item_id]}')
+        messages.success(request, f'Updated {programme.fixture} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
-        messages.success(request, f'Removed {programme.fixture} from your bag')
+        messages.success(request, f'Removed {programme.fixture} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -48,13 +50,17 @@ def adjust_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """ removes specified programme from the shopping cart """
     try:
+        programme = get_object_or_404(Programme, pk=item_id)
         cart = request.session.get('cart', {})
+        print(cart)
+        print(item_id)
 
         cart.pop(item_id)
-        messages.success(request, f'Removed {programme.fixture} from your bag')
+        messages.success(request, f'Removed {programme.fixture} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
