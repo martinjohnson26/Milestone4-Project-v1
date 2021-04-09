78157039df57ab5d9555be70b3d2f1c11a2854a5
martinjohnson26 <martin.johnson26@outlook.com>
2021-04-09 13:12:49 +0000

    added basic stripe checkout functionality but payment field is not active

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IeIuJLo2DoyD1o4qa8eRrxRxJ8ELT22rBObMY2Agsw9nBn4w5Kqq6ff1f5vz4ujapJ8zrBvyIMLnMTvmWcSv3ok00Kt8JyQUS',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
