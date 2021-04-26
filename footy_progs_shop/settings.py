e2d9b26fa96d5e86b7845ffdc9a7001f0e0f5e32
martinjohnson26 <martin.johnson26@outlook.com>
2021-04-26 20:00:54 +0000

    heroku deployment part2

--- footy_progs_shop/settings.py
+++ footy_progs_shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_wze^^#bf)*-l&+-3zw)@90_#3817a8e4yi1cn$_j96x&lda!4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone4project.herokuapp.com','localhost']
 
 
 # Application definition
@@ -120,12 +120,18 @@ WSGI_APPLICATION = 'footy_progs_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-      }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 
